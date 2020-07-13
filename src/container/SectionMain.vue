bf01e31389cfd5afab9ce1f095c918d60c0402ac
Alexey Horodesky <gailers@mail.ru>
2020-07-13 22:54:36 +0300

    bg

---
+++ src/container/SectionMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container-main">
+     
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.container-main {
+  width: 1920px;
+  height: 835px;
+  background: url('../assets/images/header_image_rectangle.png') no-repeat;
+  background-position: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <main>
     <Header/>
+    <SectionMain/>
   </main>
 </template>
 
 <script>
 import Header from '../components/header/Header'
+import SectionMain from '../container/SectionMain'
 export default {
  name: 'Home',
  components: {
     Header,
+    SectionMain,
  }
 }
 </script>
