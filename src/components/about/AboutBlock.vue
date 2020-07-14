78c1af1d41311d75346ae55bc6bf5a4c8d77d800
Alexey Horodesky <gailers@mail.ru>
2020-07-14 21:07:20 +0300

    add components AboutItem<AboutBlock and reset Aside components & ContainerAbout

---
+++ src/components/about/AboutBlock.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="about-block__wrapper-item">
+   <AboutItem
+    v-for="InfoItem in InfoItems"
+    :key="InfoItem.article"
+    :InfoItem_data="InfoItem"
+    />
+  </div>
+</template>
+
+<script>
+import AboutItem from '../about/item/AboutItem'
+
+export default {
+ name: 'AboutBlock',
+ data(){
+   return{
+    InfoItems:[
+      {
+        images:"internet.png",
+        title: "Awesome Icons",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+        article:"T1",
+      },
+      {
+        images:"transfer.png",
+        title: "One Page",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+        article:"T2",
+      },
+      {
+        images:"head.png",
+        title: "Fully Responsive",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+        article:"T3",
+      }
+    ]
+   }
+ },
+ 
+ components: {
+   AboutItem,
+ }
+};
+</script>
+
+<style lang="scss" scoped>
+.about-block__wrapper-item {
+  display: flex;
+  justify-content: space-between;
+  width: 1060px;
+  height: 200px;
+  padding-top: 125px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/about/item/AboutItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="about-wrapper__item">
+     <img class="item__images" :src=" require('../../../assets/images/' +  InfoItem_data.images)" alt="img">
+     <h3 class="item__title">{{ InfoItem_data.title }}</h3>
+     <p class="item__text">{{ InfoItem_data.text }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+ name:'AboutItem',
+ props:{
+  InfoItem_data: {
+     type: Object,
+     default(){
+        return{
+
+        }
+     }
+  }
+ },
+ data(){
+    return{
+
+    }
+ },
+ computed:{
+
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+.about-wrapper__item{
+   width: 260px;
+}
+.item__images{
+   display: flex;
+   margin: 0 auto;
+   margin-bottom: 40px;
+}
+.item__title{
+  width: 260px;
+  height: 19px;
+  font-family: 'OpenSans';
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 1.1;
+  letter-spacing: 1px;
+  text-align: center;
+  color: #212121;
+  margin-bottom: 26px;
+}
+.item__text {
+  width: 254px;
+  height: 60px;
+  font-family: 'OpenSans';
+  font-size: 15px;
+  line-height: 1.47;
+  letter-spacing: 0.75px;
+  text-align: center;
+  color: #212121;
+  margin: 0 auto;
+}
+</style>
