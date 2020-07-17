18228fe390318ce406d0e454daf74cca721c20a6
Alexey Horodesky <gailers@mail.ru>
2020-07-17 04:05:11 +0300

    add components Team, Blog

---
+++ src/container/ContainerBlog.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="blog">
+    <div class="blog__container">
+      <ArticleTitle title="Blog" />
+      <ArticleText
+        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+      />
+      <BlogPost/>
+    </div>
+  </section>
+</template>
+
+<script>
+import ArticleTitle from '../components/section/ArticleTitle'
+import ArticleText from '../components/section/ArticleText'
+import BlogPost from '../components/blog/BlogPost'
+
+export default {
+ name: 'ContainerBlog',
+ components:{
+  ArticleTitle,
+  ArticleText,
+  BlogPost,
+ }
+ 
+};
+</script>
+
+<style lang="scss">
+.blog {
+  display: flex;
+  justify-content: center;
+  width: 1920px;
+  height: 1869px;
+  background-color: #e4fdf9;
+}
+.blog__container {
+  width: 1170px;
+  height: 1620px;
+  padding-top: 125px;
+  margin-bottom: 125px;
+}
+</style>
---
+++ src/container/ContainerTeam.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="team">
+    <div class="team__container">
+      <ArticleTitle title="Our Team Member" />
+      <ArticleText
+        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+      />
+      <TeamGroup/>
+    </div>
+  </section>
+</template>
+
+<script>
+import ArticleTitle from '../components/section/ArticleTitle';
+import ArticleText from '../components/section/ArticleText';
+import TeamGroup from '../components/team/TeamGroup'
+
+export default {
+  name: 'ContainerTeam',
+  components: {
+    ArticleTitle,
+    ArticleText,
+    TeamGroup,
+  },
+};
+</script>
+
+<style lang="scss">
+.team {
+  display: flex;
+  justify-content: center;
+  width: 1920px;
+  height: 1095px;
+}
+.team__container {
+  width: 1170px;
+  height: 847px;
+  padding-top: 125px;
+  margin-bottom: 125px;
+}
+</style>
