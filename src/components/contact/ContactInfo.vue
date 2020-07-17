124db009d2110d3b0845107457856dde572e775b
Alexey Horodesky <gailers@mail.ru>
2020-07-17 21:41:08 +0300

    add components form

---
+++ src/components/contact/ContactInfo.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="contact-info__item">
+    <div class="item__block">
+      <div class="br__svg"><Location /></div>
+      <p class="item__information-name">
+        Nobinagar savar,Dhaka Bangladesh
+      </p>
+    </div>
+    <div class="item__block">
+      <div class="br__svg"><Phone /></div>
+      <p class="item__information-name">
+        +8801743331996 +8801928737807
+      </p>
+    </div>
+    <div class="item__block">
+      <div class="br__svg"><Emaill /></div>
+      <p class="item__information-name">
+        quickmasud@gmail.com quickmasud@yahoo.com
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Location from '../svgs/Location';
+import Phone from '../svgs/Phone';
+import Emaill from '../svgs/Emaill';
+export default {
+  name: 'ContactInfo',
+  components: {
+    Location,
+    Phone,
+    Emaill,
+  },
+};
+</script>
+
+<style lang="scss">
+.contact-info__item {
+  display: flex;
+  justify-content: space-between;
+  width: 958px;
+  height: 197px;
+  padding: 100px 0 80px 0;
+  margin: 0 auto;
+}
+.item__block {
+  width: 270px;
+  height: 200px;
+}
+.br__svg {
+  width: 105px;
+  height: 105px;
+  background-color: #3949ab;
+  border-radius: 50%;
+  margin: 0 auto;
+  margin-bottom: 40px;
+  .location {
+    padding: 25px;
+  }
+  .phone {
+    padding: 26px;
+  }
+  .emaill {
+    padding: 26px;
+  }
+}
+.item__information-name {
+  font-family: 'Open Sans';
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 1.4;
+  letter-spacing: 1px;
+  text-align: center;
+  color: #ffffff;
+}
+</style>
---
+++ src/components/contact/feedback/Form.vue
@@ -0,0 +1,151 @@
+<template>
+  <form class="form" @submit.prevent="sendEmail">
+    <input
+      class="form-control__name"
+      placeholder="Your NAME"
+      type="name"
+      id="name"
+      name="name"
+      tabindex="1"
+      required
+      v-model="name"
+    />
+
+    <input
+      class="form-control__email"
+      placeholder="Your Email"
+      type="email"
+      id="email"
+      name="email"
+      tabindex="2"
+      required
+      v-model="email"
+    />
+
+    <textarea
+      class="form-control__message"
+      placeholder="Write Massage"
+      rows="4"
+      cols="50"
+      type="message"
+      id="message"
+      name="message"
+      tabindex="3"
+      required
+      v-model="message"
+    ></textarea>
+    <input class="send" type="submit" value="Send" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  components: {},
+  data () {
+  return {
+    name:'',
+    email: '',
+    message: ''
+  }
+},
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.form {
+  width: 1030px;
+  height: 608px;
+}
+.form-label {
+  width: 117px;
+  height: 14px;
+  opacity: 0.8;
+  font-family: 'Open Sans';
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 1.4;
+  letter-spacing: 1px;
+  text-align: left;
+  color: #212121cc;
+}
+.form-control {
+  &__message {
+    max-width: 969px;
+    width: 100%;
+    height: 323px;
+    padding: 30px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    font-family: 'Open Sans';
+    font-size: 20px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.4;
+    letter-spacing: 1px;
+    text-align: left;
+    outline: none;
+    margin-bottom: 30px;
+    resize: none;
+  }
+  &__email {
+    font-family: 'Open Sans';
+    font-size: 20px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.4;
+    letter-spacing: 1px;
+    text-align: left;
+    width: 470px;
+    padding-left: 30px;
+    height: 84px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    border-style: none none solid none;
+    box-shadow: none;
+    outline: none;
+    margin-bottom: 30px;
+  }
+  &__name {
+    font-family: 'Open Sans';
+    font-size: 20px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.4;
+    letter-spacing: 1px;
+    text-align: left;
+    width: 470px;
+    padding-left: 30px;
+    height: 84px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    border-style: none none solid none;
+    box-shadow: none;
+    outline: none;
+    margin-right: 30px;
+    margin-bottom: 30px;
+  }
+}
+.send {
+  width: 220px;
+  height: 70px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  border: none;
+  font-family: 'Roboto';
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 1.1;
+  letter-spacing: 1px;
+  text-align: center;
+  color: #212121;
+  text-transform: uppercase;
+  position: absolute;
+  left: 65.9%;
+}
+</style>
